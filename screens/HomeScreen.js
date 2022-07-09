c77e1099fb40b5fe941def491d4c2b2f4646c4b0
Vikas Jyani <58882569+VickDaPro@users.noreply.github.com>
2022-07-10 01:55:35 +0530

    feat/ added category ScrollView

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,4 +1,11 @@
-import { View, Text, SafeAreaView, Image, TextInput } from "react-native";
+import {
+  View,
+  Text,
+  SafeAreaView,
+  Image,
+  TextInput,
+  ScrollView,
+} from "react-native";
 import React, { useLayoutEffect } from "react";
 import { useNavigation } from "@react-navigation/native";
 import {
@@ -7,6 +14,8 @@ import {
   SearchIcon,
   AdjustmentsIcon,
 } from "react-native-heroicons/outline";
+
+import Categories from "../components/Categories";
 
 const HomeScreen = () => {
   const navigation = useNavigation();
@@ -19,43 +28,50 @@ const HomeScreen = () => {
 
   return (
     <SafeAreaView className="bg-white pt-5">
-      <Text className="text-red-500">
-        {/* Header */}
+      {/* Header */}
 
-        <View className="flex-row pb-3 items-center mx-4 space-x-2 px-4">
-          <Image
-            source={{
-              uri: "https://links.papareact.com/wru",
-            }}
-            className="h-7 w-7 bg-gray-300 p-4 rounded-full"
+      <View className="flex-row pb-3 items-center mx-4 space-x-2 px-4">
+        <Image
+          source={{
+            uri: "https://links.papareact.com/wru",
+          }}
+          className="h-7 w-7 bg-gray-300 p-4 rounded-full"
+        />
+
+        <View className="flex-1">
+          <Text className="font-bold text-gray-400 text-xs">Deliver Now!</Text>
+          <Text className="font-bold text-xl">
+            Current Location
+            <ChevronDownIcon size={20} color="#00ccbb" />
+          </Text>
+        </View>
+
+        <UserIcon size={35} color="#00ccbb" />
+      </View>
+
+      {/* Search */}
+      <View className="flex-row items-center space-x-2 pb-2 mx-4">
+        <View className="flex-row flex-1 space-x-2 bg-gray-200 p-3">
+          <SearchIcon color="gray" size={20} />
+          <TextInput
+            placeholder="Restaurants and cuisines"
+            keyboardType="default"
           />
-
-          <View className="flex-1">
-            <Text className="font-bold text-gray-400 text-xs">
-              Deliver Now!
-            </Text>
-            <Text className="font-bold text-xl">
-              Current Location
-              <ChevronDownIcon size={20} color="#00ccbb" />
-            </Text>
-          </View>
-
-          <UserIcon size={35} color="#00ccbb" />
         </View>
 
-        {/* Search */}
-        <View className="flex-row items-center space-x-2 pb-2 mx-4 px-4">
-          <View className="flex-row flex-1 space-x-2 bg-gray-200 p-3">
-            <SearchIcon color="gray" size={20} />
-            <TextInput
-              placeholder="Restaurants and cuisines"
-              keyboardType="default"
-            />
-          </View>
+        <AdjustmentsIcon color="#00ccbb" />
+      </View>
 
-          <AdjustmentsIcon color="#00ccbb" />
-        </View>
-      </Text>
+      {/* Body */}
+      <ScrollView
+        className="bg-gray-100"
+        contentContainerStyle={{
+          paddingBottom: 100,
+        }}
+      >
+        {/* Categories */}
+        <Categories />
+      </ScrollView>
     </SafeAreaView>
   );
 };
